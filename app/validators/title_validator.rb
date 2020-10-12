c46bcc4ab801f446932eb4bb46edde2639f655c5
see-bet-code <brittani.elan.taylor@gmail.com>
2020-10-12 10:49:53 -0400

    Done.

--- app/validators/title_validator.rb
+++ app/validators/title_validator.rb
@@ -1,8 +1,12 @@
 class TitleValidator < ActiveModel::Validator
+    CLICKBAIT_KEYWORDS = [
+            /Won't Believe/,
+            /Secret/,
+            /Guess/,
+            /Top [0-9]/
+        ]
     def validate(record)
-        res = ["Won't Believe", "Secret", "Guess"].none? do |w|
-            record.title.include? w if record.title
-        end
-        record.errors[:title] << "is not sufficiently clickbait-." if res
+        res = CLICKBAIT_KEYWORDS.none? { |w| w.match? record.title if record.title }
+        record.errors[:title] << "is not sufficiently clickbait-y." if res
     end
 end
